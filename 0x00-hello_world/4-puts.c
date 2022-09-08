2ded8d33da529f4f809ca2a733c96a73fada767a
Tsepomatlou <tsepomatlou@gmail.com>
2022-09-08 05:42:02 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+nt main(void)
+
+{
+
+	  puts("\"Programming is like building a multilingual puzzle");
+
+	    return (0);
+
+	    puts("\"Programming is like building a multilingual puzzle");
+
+	    return (0);
+
+}
