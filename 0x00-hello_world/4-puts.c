6e53cd8aeba0ae73aca3a405fb5712225fe61914
Tsepomatlou <tsepomatlou@gmail.com>
2022-09-12 03:24:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,19 @@
-nt main(void)
+#include <stdio.h>
+
+/**
+ *
+ *  * Description: using the main function - Prints out a string to stdout.
+ *
+ *   * Return: 0 if success.
+ *
+ *    */
+
+int main(void)
 
 {
 
-	  puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-	    return (0);
-
-	    puts("\"Programming is like building a multilingual puzzle");
-
-	    return (0);
+	return (0);
 
 }
