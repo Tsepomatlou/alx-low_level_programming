c2f71e1f5a43f99716dc66453f5ddb33ead5ba1c
Tsepomatlou <tsepomatlou@gmail.com>
2022-09-22 02:14:44 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,37 @@
+#include (betty.h)
+
+/*
+ *
+ *     main - Entry point
+ *
+ *   
+ *
+ *     Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+
+		char *sh = "Betty";
+
+
+
+			while (*sh)
+
+					{
+
+								_putchar(*sh);
+
+										sh++;
+
+											}
+
+				_putchar('\n');
+
+
+
+					return (0);
+
+}
